6f94d116916bec7f6e491ec313f950187363af60
EEEDU <edu7guerrero@gmail.com>
2026-09-05 11:48:52 +0200

    REFACTOR: dividir elementos y hacer un refactor en general

---
+++ app/src/main/java/com/example/todots/ui/theme/Dimens.kt
@@ -0,0 +1,9 @@
+package com.example.todots.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val CornerRadius = 16.dp
+    val PaddingLista = 32.dp
+    val PaddingItem = 12.dp
+}
